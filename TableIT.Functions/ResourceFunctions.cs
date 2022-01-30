8cbc6773ef669d0033ef2a37c7864ac7516134e6
Kevin Bost <kitokeboo@gmail.com>
2022-01-29 23:57:43 -0800

    Adding import function

--- TableIT.Functions/ResourceFunctions.cs
+++ TableIT.Functions/ResourceFunctions.cs
@@ -23,49 +23,56 @@ namespace TableIT.Functions
             [Blob("resources/{resourceId}", FileAccess.Read, Connection = "BlobConnection")] CloudBlob blob,
             ILogger log)
         {
-
-            if (!await blob.ExistsAsync())
+            try
             {
-                return new NotFoundResult();
-            }
+                if (!await blob.ExistsAsync())
+                {
+                    return new NotFoundResult();
+                }
 
-            if (TryGetQueryParameter("width", out int? width) |
-                TryGetQueryParameter("height", out int? height))
-            {
-                using var ms = new MemoryStream();
-                await blob.DownloadToStreamAsync(ms);
-                ms.Position = 0;
-                using var bitmap = SKBitmap.Decode(ms);
-                SKImageInfo imageInfo;
-                if (width is not null && height is not null)
+                if (TryGetQueryParameter("width", out int? width) |
+                    TryGetQueryParameter("height", out int? height))
                 {
-                    imageInfo = new SKImageInfo(width.Value, height.Value);
-                }
-                else if (height is not null)
-                {
-                    int thumbnailWidth = (int)((bitmap.Height / (double)bitmap.Width) * height.Value);
-                    imageInfo = new SKImageInfo(thumbnailWidth, height.Value);
-                }
-                else //if (width is not null)
-                {
-                    int thumbnailHeight = (int)((bitmap.Width / (double)bitmap.Height) * width.Value);
-                    imageInfo = new SKImageInfo(width.Value, thumbnailHeight);
-                }
-                using var resized = bitmap.Resize(imageInfo, SKFilterQuality.Medium);
-                SKData resizedData = resized.Encode(SKEncodedImageFormat.Jpeg, 100);
+                    using var ms = new MemoryStream();
+                    await blob.DownloadToStreamAsync(ms);
+                    ms.Position = 0;
+                    using var bitmap = SKBitmap.Decode(ms);
+                    SKImageInfo imageInfo;
+                    if (width is not null && height is not null)
+                    {
+                        imageInfo = new SKImageInfo(width.Value, height.Value);
+                    }
+                    else if (height is not null)
+                    {
+                        int thumbnailWidth = (int)((bitmap.Height / (double)bitmap.Width) * height.Value);
+                        imageInfo = new SKImageInfo(thumbnailWidth, height.Value);
+                    }
+                    else //if (width is not null)
+                    {
+                        int thumbnailHeight = (int)((bitmap.Width / (double)bitmap.Height) * width.Value);
+                        imageInfo = new SKImageInfo(width.Value, thumbnailHeight);
+                    }
+                    using var resized = bitmap.Resize(imageInfo, SKFilterQuality.Medium);
+                    SKData resizedData = resized.Encode(SKEncodedImageFormat.Jpeg, 100);
 
-                return new FileStreamResult(resizedData.AsStream(true), blob.Properties.ContentType)
+                    return new FileStreamResult(resizedData.AsStream(true), blob.Properties.ContentType)
+                    {
+                        EntityTag = new Microsoft.Net.Http.Headers.EntityTagHeaderValue(blob.Properties.ETag)
+                    };
+                }
+
+                var blobStream = await blob.OpenReadAsync();
+                var result = new FileStreamResult(blobStream, blob.Properties.ContentType)
                 {
                     EntityTag = new Microsoft.Net.Http.Headers.EntityTagHeaderValue(blob.Properties.ETag)
                 };
+                return result;
             }
-
-            var blobStream = await blob.OpenReadAsync();
-            var result = new FileStreamResult(blobStream, blob.Properties.ContentType)
+            catch(Exception ex)
             {
-                EntityTag = new Microsoft.Net.Http.Headers.EntityTagHeaderValue(blob.Properties.ETag)
-            };
-            return result;
+                log.LogError(ex, "Error getting resource {0}", req.Query);
+                throw;
+            }
 
             bool TryGetQueryParameter(string name, [NotNullWhen(true)] out int? value)
             {
@@ -86,19 +93,55 @@ namespace TableIT.Functions
             [StorageAccount("BlobConnection")] CloudStorageAccount account,
             ILogger log)
         {
-            CloudBlobClient blobClient = new(account.BlobEndpoint, account.Credentials);
-            var container = blobClient.GetContainerReference("resources");
-
-            List<Resource> resources = new();
-            foreach (CloudBlob blob in container.ListBlobs(useFlatBlobListing: true, blobListingDetails: BlobListingDetails.Metadata))
+            try
             {
-                if (!blob.Metadata.TryGetValue("DisplayName", out string displayName))
+                CloudBlobClient blobClient = new(account.BlobEndpoint, account.Credentials);
+                var container = blobClient.GetContainerReference("resources");
+
+                List<Resource> resources = new();
+                foreach (CloudBlob blob in container.ListBlobs(useFlatBlobListing: true, blobListingDetails: BlobListingDetails.Metadata))
                 {
-                    displayName = blob.Name;
+                    if (!blob.Metadata.TryGetValue("DisplayName", out string displayName))
+                    {
+                        displayName = blob.Name;
+                    }
+                    resources.Add(new Resource(blob.Name, displayName, blob.Properties.ETag));
                 }
-                resources.Add(new Resource(blob.Name, displayName, blob.Properties.ETag));
+                return new JsonResult(resources);
             }
-            return new JsonResult(resources);
+            catch (Exception ex)
+            {
+                log.LogError(ex, "Error listing resorces");
+                throw;
+            }
+        }
+
+        [FunctionName("ImportResource")]
+        public static async Task<IActionResult> ImportResources(
+                [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "import/resources")] HttpRequest req,
+                [StorageAccount("BlobConnection")] CloudStorageAccount account,
+                ILogger log)
+        {
+            try
+            {
+                IFormCollection form = await req.ReadFormAsync();
+                IFormFile file = form.Files["file"];
+                
+                CloudBlobClient blobClient = account.CreateCloudBlobClient();
+                var container = blobClient.GetContainerReference("resources");
+
+                CloudBlockBlob blob = container.GetBlockBlobReference(file.Name + Guid.NewGuid().ToString());
+                await blob.UploadFromStreamAsync(file.OpenReadStream());
+                blob.Metadata["DisplayName"] = file.Name;
+                blob.SetMetadata();
+
+                return new JsonResult(new Resource(blob.Name, file.Name, blob.Properties.ETag));
+            }
+            catch(Exception e)
+            {
+                log.LogError(e, "Error importing resource");
+                throw;
+            }
         }
     }
 }
